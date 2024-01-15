7f94cf872f3e5f0f2252caf6aaaa80a21738f298
Read-Scoorp <fayamamani@unsa.edu.pe>
2024-01-14 21:44:00 -0500

    ERROR EN EL RECONOCIMIENTO DE VOZ

--- AC/ProjectAC.py
+++ AC/ProjectAC.py
@@ -1,11 +1,51 @@
 import pyttsx3 as con
+import openai
+import speech_recognition as reconoce
 import json
+
+#KEY API
+openai.api_key = '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'
+
+#CONVERT
+convert = reconoce.Recognizer()
+
 #CONVIERTE EN AUDIO, LAS CADENAS DE TEXTO
 def STAU (ingreso):
     palabra = con.init()
     palabra.setProperty('rate',150)
     palabra.say(ingreso)
     palabra.runAndWait()
+
+#PETICIONES DIFERENTES
+def CUE (quetion):
+    answer = openai.Completion.create(
+            engine = "text-davinci-002",
+            prompt = quetion,
+            max_tokens = 100,
+            temperature = 0.6
+            )
+    return answer['choices'][0]['text']
+    
+#RECONOCE LA VOZ
+def VOZ (valor):
+    with reconoce.Microphone() as sr:
+        if(valor):
+            STAU ('PORFAVOR INGRESE SU PETICION')
+        else:
+            STAU ('POR FAVOR COMENTE SU RESPUESTA')
+        song = convert.listen(sr)
+        STAU ('OKEY EN PROCESO')
+    
+    try:
+        text_song = convert.recognize_google(song)
+        STAU('SU PETICION SE HA TRANSCRITO EN LA TERMINAL')
+        print ("SU PETICION: ", text_song)
+        return text_song
+    except reconoce.UnknownValueError:
+        STAU('No se reconoce correctamente su peticion por audio')
+    except reconoce.RequestError as e:
+        print("Error en la solicitud a Google Speech Recognition: {0}".format(e))
+
 #ENLACE BASE DE DATOS
 with open('DATA.json', 'r') as archivo:
     datos =  json.load(archivo)
@@ -13,3 +53,5 @@ with open('DATA.json', 'r') as archivo:
 #MAIN: FUNCION PRINCIPAL
 if __name__ == "__main__":
     STAU(datos['welcome'])
+    nombre = VOZ(False)
+    print ('tu nombre es ', nombre)
